3ee42b9d285efea413ed07106cdfbd6e0a0df55b
JesperKYH <jesper.pettersson@student.kyh.se>
2021-01-21 10:27:03 +0100

    Changed about me text

--- src/components/About.js
+++ src/components/About.js
@@ -15,18 +15,15 @@ const About = () => {
             height="350"
           />
           <Text>
-            My name is Jesper Pettersson. I'm currently studying to become a
-            front-end developer at KYH in Stockholm. When I was a teen I found
-            out that you could code your own games so I wanted to try that. I
-            was trying to learn game development by myself but it did not go my
-            way, so my fun in programming faded.
-            <br /> <br /> But after i was done with school i started working and
-            after a year I wanted to study something again and then I found
-            front-end development. From the start I knew very little about
-            programming and web development, I thought we would learn C# at KYH
-            because I had very small idea what front-end development was.
-            Nowadays I know alot more. <br /> <br />
-            You can watch my projects down below and also see my github repos.
+            Hello! My name is Jesper Pettersson. I'm currently studying to become a
+            front-end developer at KYH in Stockholm. I have always had an interest in
+            programming but it wasn't until recently, in 2020, that I decided to actually
+            study programming and try to become a front-end developer. <br /> <br />
+            I already have knowledge in JavaScript and React, but my next goal is to
+            learn Redux and Python.<br /> <br />
+            Down below you can find my finished work and I also have a Github page
+            where you can find a list of my repos. I look forward to hearing from
+            you and I hope we can achieve amazing things together!
           </Text>
         </Wrapper2>
       </Section>
